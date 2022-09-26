fc24f9a3056d6a8b901c576672a71d9da59b4620
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-26 16:10:22 +0300

    side bar calculation completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,6 +10,16 @@ const Cart = ({cart}) => {
     }
 
     //finding the shipping charge from the cart array
+    let shippingCharge = 0;
+    for(let product of cart){
+        shippingCharge = shippingCharge + product.shipping;
+    }
+
+    //finding the total tax amount from the cart array
+    let tax = 0;
+    tax = parseFloat((totalPrice * 0.1).toFixed(2));
+    //finding grand total
+    const grandTotal = totalPrice + shippingCharge + tax;
     
     return (
         <div className='sticky top-0 p-10'>
@@ -25,20 +35,18 @@ const Cart = ({cart}) => {
                 </div>
                 <div className='flex justify-between'>
                     <p>Total Shipping Charge:</p>
-                    <p className='font-bold'>$0</p>
+                    <p className='font-bold'>${shippingCharge}</p>
                 </div>
                 <div className='flex justify-between'>
                     <p>Tax:</p>
-                    <p className='font-bold'>$0</p>
+                    <p className='font-bold'>${tax}</p>
                 </div>
                 <div className='border-b-2 border-slate-400'></div>
                 <div className='flex justify-between mt-2 items-center'>
-                    <p className='text-2xl font-semibold'>Grand Total:</p>
-                    <p className='text-2xl font-semibold'>$0</p>
+                    <p className='text-xl font-semibold'>Grand Total:</p>
+                    <p className='text-xl font-semibold'>${grandTotal.toFixed(2)}</p>
                 </div>
-                
             </div>
-           
         </div>
     );
 };
