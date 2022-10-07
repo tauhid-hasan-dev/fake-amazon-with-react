8ac4672780ecc53b7e5af1c7eb11f829540c707a
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-07 18:18:58 +0300

    products in orders router added and styled

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const {products, initialCart} = useLoaderData();
     const [cart, setCart] = useState(initialCart)
     return (
         <div className='grid grid-cols-5'>
-            <div className="products col-span-5 lg:col-span-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-12 p-20 ">
-                
+            <div className="products col-span-5 lg:col-span-4 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-5  lg:gap-8 p-5 lg:p-20 lg:px-40 order-2 lg:order-1">
+                {
+                   cart.map(product => <ReviewItem 
+                                            key={product.id} 
+                                            product = {product}>
+                                       </ReviewItem>)
+                }
             </div>
-            <div className="products bg-cart-color">
+            <div className="products bg-cart-color col-span-5 lg:col-span-1 order-1 lg:order-2">
                 <Cart cart={cart}></Cart>
             </div>
         </div>
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+const ReviewItem = ({product}) => {
+    const {name, quantity, price, shipping,  img} = product;
+    return (
+        <div className='flex  border-2 gap-5 rounded-lg p-2'>
+            <div className='w-28 lg:w-36'>
+                <img src={img} alt="" />
+            </div>
+            <div className='flex   items-center justify-between w-full'>
+                <div>
+                    <p className='text-sm lg:text-2xl font-semibold'>{name}</p>
+                    <p className='text-sm lg:text-lg'>Price: ${price}</p>
+                    <p className='text-sm lg:text-lg'>Shipping Charge: ${shipping}</p>
+                    <p  className='text-sm lg:text-lg'>Quantity: {quantity}</p>
+                </div>
+                <div className='text-2xl lg:text-5xl  text-red-500  hover:text-red-700 cursor-pointer  p-2 lg:p-5 '>
+                    <FontAwesomeIcon icon={faTrash} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewItem;
