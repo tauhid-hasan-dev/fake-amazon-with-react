ba4b16621751de09eeb65bc625c68083c92dd1b2
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-07 16:44:25 +0300

    cart component cloned to orders component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 
 const Orders = () => {
     const {products, initialCart} = useLoaderData();
     const [cart, setCart] = useState(initialCart)
     return (
-        <div>
-            <p>this is orders : {products.length}</p>
-            <p>Initital cart : {cart.length}</p>
+        <div className='grid grid-cols-5'>
+            <div className="products col-span-5 lg:col-span-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-12 p-20 ">
+                
+            </div>
+            <div className="products bg-cart-color">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
