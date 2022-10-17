10e33a160c29fab6c4e6c62389308e57060a9cf5
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-17 15:57:16 +0300

    getting form value

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const handleSubmit = (event)=> {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+
+    }
     return (
         <div className='p-20  flex flex-col items-center'>
-            <form className="p-10 shadow-2xl w-[450px] ">
+            <form className="p-10 shadow-2xl w-[450px] " onSubmit={handleSubmit}>
                     <p className='text-center text-3xl font-regular'>Sign Up</p>
                     <div className="form-control">
                         <label className="label">
