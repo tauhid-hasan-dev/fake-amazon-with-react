47453908c2c63c1bd4fc218b9bce8c4a230af7a6
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-17 14:25:05 +0300

    daisy ui hero login form imported and customized

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <p>this is login</p>
+        <div className='p-20  flex flex-col items-center'>
+            <form className="p-10 shadow-2xl w-[450px] ">
+                    <div className="form-control">
+                        <label className="label">
+                                 <span className="label-text">Email</span>
+                         </label>
+                    <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <Link to='' className="label-text-alt link link-hover">Forgot password?</Link>
+                         </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+            </form>
         </div>
+       
+                
+          
     );
 };
 
